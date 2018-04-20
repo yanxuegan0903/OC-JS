bfb7970a52b83b5307cc3105e68703e422bcef8d
[username] <yanxuegan0903@163.com>
2018-04-20 10:10:28 +0800

    Initial Commit

---
+++ TestOC&JS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOC&JS
+//
+//  Created by vsKing on 2018/4/20.
+//  Copyright © 2018年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOC&JSTests/TestOC_JSTests.m
@@ -0,0 +1,39 @@
+//
+//  TestOC_JSTests.m
+//  TestOC&JSTests
+//
+//  Created by vsKing on 2018/4/20.
+//  Copyright © 2018年 vsKing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestOC_JSTests : XCTestCase
+
+@end
+
+@implementation TestOC_JSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
